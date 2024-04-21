083a4d51a81f3af5c4559d597131042d937e4762
Madjskz <sergejnasledov5@gmail.com>
2024-04-21 17:22:16 +0500

    beta 1.0

--- main.py
+++ main.py
@@ -6,132 +6,123 @@ import websockets
 from textual import work, events
 from textual.app import App, ComposeResult
 from textual.containers import ScrollableContainer, Container, Horizontal
-from textual.widgets import Footer, Header, Static, Label, Input
+from textual.screen import Screen
+from textual.widgets import Footer, Header, Static, Label, Input, Button
+
 
 URI: str = "ws://localhost:8765"
 
-app_is_closing: bool = False
 
-users_list: list = []
-messages_list: list = []
-
-current_user_id = 1
-
-waiting_message = []
-
-
-class Message(Static):
-    def __init__(self, id: int = -1, username: str = 'user', text: str = 'text', date: str = 'date'):
-        super().__init__()
-        self.id_message = id
-        self.username = username
-        self.text = text
-        self.date = date
-
-    def compose(self):
-        yield Label(self.username, id=f'user_field')
-        yield Label(self.text, id='message_field')
-        yield Label(self.date, id='date_field')
+class Authorization_screen(Screen):
+    def compose(self) -> ComposeResult:
+        yield Container(
+            Label('Authorization', id='auth_label'),
+            Input(id='login', placeholder='Login', classes='auth_input'),
+            Input(id='password', placeholder='Password', classes='auth_input'),
+            Button(id='auth_button', label='Send'),
+            id='dialog_auth')
 
 
-class User(Static):
-    def __init__(self, id_user: int = -1, username: str = 'user', online_status: bool = False):
-        super().__init__()
-        self.id_user = id_user
-        self.username = username
-        self.online_status = online_status
+class Chat_screen(Screen):
+    def compose(self) -> ComposeResult:
+        yield Header(show_clock=True)
 
-        self.label = None
+        yield Footer()
 
-    def compose(self):
-        self.label = Label(self.username, id=f'user_online_field')
-        self.label.styles.visibility = 'visible' if self.online_status else 'hidden'
+        user_container = ScrollableContainer(id='user_container')
+        user_container.styles.width = 20
+        user_container.styles.scrollbar_size_vertical = 1
+        user_container.styles.scrollbar_color = "#737373"
+        user_container.styles.scrollbar_color_active = "#817D74"
+        user_container.styles.scrollbar_color_hover = "#817D74"
 
-        yield self.label
+        message_container = ScrollableContainer(id='message_container')
+        message_container.styles.scrollbar_size_vertical = 1
+        message_container.styles.scrollbar_color = "#737373"
+        message_container.styles.scrollbar_color_active = "#817D74"
+        message_container.styles.scrollbar_color_hover = "#817D74"
 
-    def change_online_status(self, online_status):
-        self.online_status = online_status
-        self.label.styles.visibility = 'visible' if online_status else 'hidden'
+        yield Container(
+            Horizontal(
+                user_container,
+                message_container
+            )
+        )
+
+        yield Input(id='input_message', placeholder='Введите сообщение')
 
 
 class ChatApp(App):
-    DEFAULT_CSS = """
-    Message {
-        margin: 1;
-        border: solid red;
-        text-style: italic;
-    }
-    
-    User {
-        margin: 1
-    }
-    
-    #message {
-        background: $boost;
-    }
-    
-    #input {
-        background: grey;
-    }
-    
-    #user_field {
-        text-style: underline;
-    }
-    
-    #message_field {
-        
-    }
-    
-    #date_field {
-        text-style: bold;
-    }
-    """
+    CSS_PATH = 'main.tcss'
 
     BINDINGS = [
         ("q", "close", "Close"),
     ]
 
+    MODES = {
+        'authorization': Authorization_screen,
+        'chat': Chat_screen
+    }
+
+    def on_mount(self) -> None:
+        self.switch_mode('authorization')
+        self.title = "MAGNET1C H1LLS CHAT"
+
     def __init__(self):
         super().__init__()
-        self.is_close = False
-        self.message_wait_send = []
+        self.send_auth: bool = False
+        self.is_close: bool = False
+
+        self.login: str = ''
+        self.password: str = ''
+
+        self.current_user_id = None
+
+        self.message_wait_send: list = []
+        self.users_list: list = []
+        self.messages_list: list = []
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == 'auth_button':
+            self.login = self.query_one('#login').value
+            self.password = self.query_one('#password').value
+
+            self.send_auth = True
 
     async def on_load(self) -> None:
         self.websocket_start()
 
     @work(exclusive=True, thread=True)
-    async def websocket_start(self):
-        global app_is_closing
+    async def websocket_start(self) -> None:
+        while not self.send_auth:
+            await asyncio.sleep(0.2)
 
         async with websockets.connect(URI) as websocket:
             await websocket.send(json.dumps({
-                'username': 'user1',
-                'password': 'user1'
+                'username': self.login,
+                'password': self.password
             }))
 
-            await websocket.recv()
+            self.current_user_id = int(str(await websocket.recv()).split()[0])
+
+            await self.switch_mode('chat')
 
             await asyncio.gather(self.listen_server(websocket),
                                  self.send_message_on_server(websocket))
 
-    async def send_message_on_server(self, websocket: websockets):
-        global waiting_message
+    def on_input_submitted(self, event: Input.Submitted):
+        if event.input.id == 'input_message':
+            self.message_wait_send.append(json.dumps({
+                'OwnerID': self.current_user_id,
+                'Message': event.input.value.strip()
+            }))
 
+            self.query_one(Input).value = ''
+
+    async def listen_server(self, websocket: websockets) -> None:
         while True:
-            if len(waiting_message):
-                for message in waiting_message:
-                    await websocket.send(message)
-
-                waiting_message.clear()
-
-            await asyncio.sleep(0.1)
-
-    async def listen_server(self, websocket: websockets):
-        while True:
-            if app_is_closing:
-                self.query_one(Input).value = str(app_is_closing)
+            if self.is_close:
                 await websocket.close()
-                await websocket.send('close')
 
             try:
                 recv = await asyncio.wait_for(websocket.recv(), timeout=1)
@@ -154,81 +145,77 @@ class ChatApp(App):
             except TimeoutError:
                 pass
 
-    async def get_new_message(self, js: json):
-        global messages_list
+    async def get_new_message(self, js: json) -> None:
+        self.messages_list.append(Message(js['ID'], await self.find_username(js['OwnerID']),
+                                          js['Message'], js['Date']))
 
-        messages_list.append(Message(js['ID'], await self.find_user(js['OwnerID']),
-                                     js['Message'], js['Date']))
+        await self.query_one("#message_container").mount(self.messages_list[-1])
 
-        await self.query_one("#message_container").mount(messages_list[-1])
-        messages_list[-1].scroll_visible(speed=0.0000000001)
-
-    async def find_user(self, id: int):
-        global users_list
-
-        for user in users_list:
+    async def find_username(self, id: int) -> str:
+        for user in self.users_list:
             if user.id_user == id:
                 return user.username
 
-    async def get_online_status(self, js: json):
-        global users_list
+    async def get_online_status(self, js: json) -> None:
+        for i in range(len(self.users_list)):
+            if self.users_list[i].id_user == js['ID']:
+                self.users_list[i].change_online_status(js['OnlineStatus'])
 
-        for i in range(len(users_list)):
-            if users_list[i].id_user == js['ID']:
-                users_list[i].change_online_status(js['OnlineStatus'])
+    async def get_new_user(self, js: json) -> None:
+        self.users_list.append(User(js['ID'], js['Name'], js['OnlineStatus']))
 
-        # if
-        # for message in self.query(Message):
-        #    if message.id_message == js['ID']:
-        #        await message.remove()
+        await self.query_one("#user_container").mount(self.users_list[-1])
 
-    async def get_new_user(self, js: json):
-        global users_list
-
-        users_list.append(User(js['ID'], js['Name'], js['OnlineStatus']))
-
-        await self.query_one("#user_container").mount(users_list[-1])
-        users_list[-1].scroll_visible(speed=0.0000000001)
-
-    async def delete_message(self, js: json):
-        global messages_list
-
+    async def delete_message(self, js: json) -> None:
         await self.query_one(f'message_{js['ID']}').remove()
 
-    def compose(self) -> ComposeResult:
-        """Called to add widgets to the app."""
-        yield Header(name='MAGNET1C H1LLS CHAT')
-        yield Footer()
+    async def send_message_on_server(self, websocket: websockets) -> None:
+        while True:
+            if len(self.message_wait_send):
+                for message in self.message_wait_send:
+                    await websocket.send(message)
 
-        user_container = ScrollableContainer(id='user_container')
-        user_container.styles.width = 20
+                self.message_wait_send.clear()
 
-        message_container = ScrollableContainer(id='message_container')
+            await asyncio.sleep(0.1)
 
-        yield Container(
-            Horizontal(
-                user_container,
-                message_container
-            )
-        )
-
-        yield Input(id='input', placeholder='Введите сообщение')
-
-    def key_enter(self, event: events.Key):
-        global waiting_message
-
-        waiting_message.append(json.dumps({
-            'OwnerID': current_user_id,
-            'Message': self.query_one(Input).value
-        }))
-
-        self.query_one(Input).value = ''
-
-    def action_close(self):
-        global app_is_closing
-
-        app_is_closing = True
+    def action_close(self) -> None:
+        self.is_close = True
         self.bell()
+
+
+class Message(Static):
+    def __init__(self, id_message: int = -1, username: str = 'user', text: str = 'text', date: str = 'date'):
+        super().__init__()
+        self.id_message = id_message
+        self.username = username
+        self.text = text
+        self.date = date
+
+    def compose(self):
+        yield Static(self.username, id='user_field')
+        yield Static(self.text, id='message_field')
+        yield Static(self.date, id='date_field')
+
+
+class User(Static):
+    def __init__(self, id_user: int = -1, username: str = 'user', online_status: bool = False):
+        super().__init__()
+        self.id_user = id_user
+        self.username = username
+        self.online_status = online_status
+
+        self.static = None
+
+    def compose(self):
+        self.static = Static(self.username, id=f'user_online_field_{self.id_user}')
+        self.static.styles.color = 'red'
+
+        yield self.static
+
+    def change_online_status(self, online_status):
+        self.online_status = online_status
+        self.query_one(f'#user_online_field_{self.id_user}').styles.color = 'green' if self.online_status else 'red'
 
 
 if __name__ == "__main__":
